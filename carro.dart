64daa279a5601deb9195686fdb11c3701d1b8fe1
Carlos Eduardo Ferreira <cefs2@aluno.ifal.edu.br>
2023-03-31 19:38:39 -0300

    subindo arquivos

---
+++ carro.dart
@@ -0,0 +1,29 @@
+class Carro {
+  String modelo;
+  String marca;
+  String chassi;
+  String fabricante;
+
+  Carro({
+    required this.modelo,
+    required this.marca,
+    required this.chassi,
+    required this.fabricante,
+  });
+
+  void ligar() {
+    print("$modelo ligado");
+  }
+
+  void desligar() {
+    print("$modelo desligado");
+  }
+
+  void acelerar() {
+    print("$modelo acelerando...");
+  }
+
+  void frear() {
+    print("$modelo freando...");
+  }
+}
---
+++ main.dart
@@ -0,0 +1,36 @@
+import 'carro.dart'; //importando a classe com estruturas criadas
+
+void main() {
+  Carro onix = Carro(
+      modelo: "Onix",
+      marca: "Chevrolet",
+      chassi: "oni001",
+      fabricante: "Chevrolet");
+
+  onix.ligar();
+  onix.desligar();
+  onix.acelerar();
+  onix.frear();
+
+  Carro gol = Carro(
+      modelo: "Gol",
+      marca: "Volksvagem",
+      chassi: "gol001",
+      fabricante: "Volksvagem");
+
+  gol.ligar();
+  gol.desligar();
+  gol.acelerar();
+  gol.frear();
+  
+  Carro argo = Carro(
+      modelo: "Argo", 
+      marca: "Fiat", 
+      chassi: "arg001", 
+      fabricante: "Fiat");
+
+  argo.ligar();
+  argo.desligar();
+  argo.acelerar();
+  argo.frear();
+}
